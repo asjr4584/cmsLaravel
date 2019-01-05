9a3806df481d75680490ac35428aaaef262a4b6b
WINDOWS10-ALLAN\Allan <asjr4584@gmail.com>
2019-01-05 16:38:37 +0900

    creating controllers, routing controllers, passing data, resource and controllers

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,46 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+
+add this path to environment varialble to get access to: php artisan:
+C:\Users\Allan\AppData\Roaming\Composer\vendor\bin;C:\wamp64\bin\php\php7.3.0
+
+php artisan serve
+http://127.0.0.1:8000/
+
+php artisan serve --host=cms.test --port=80
+
+php artisan serve --host=cms.test --port=8000
+
+php artisan serve --host=cms.test --port=8080
+
+Allan@Windows10-Allan MINGW64 /
+$ cd c:/wamp64/www/cms
+
+
+creating a controller (for classes)
+Allan@Windows10-Allan MINGW64 /c/wamp64/www/cms (develop)
+$ php artisan make:controller Tutorial
+Controller created successfully.
+
+
 */
 
 //C:\wamp64\www\cms\routes
 
 //Route::view('/', 'first');
+
+Route::resource('posts', 'PostsController');
+
+Route::get('/post/{id}', 'PostsController@index');
+
+/*Route::get('/post/{id}', function ($id) {
+    //return view('welcome');
+
+    return "This post is page " . $id;
+    
+});*/
+
 
 Route::get("/tutorial/{id}", 'Tutorial@show');
 
@@ -37,12 +72,4 @@ Route::get('/about', function () {
     return "About page";
 	
 });
-
-Route::get('/post/{id}', function ($id) {
-    //return view('welcome');
-
-    return "This post is page " . $id;
-	
-});
-
 
